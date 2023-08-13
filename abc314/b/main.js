51056ab51e9ee49ab3c58dafad417584bf420ab3
Tsubasa Hoshi <hoshi1997.283@gmail.com>
2023-08-13 23:29:54 +0900

    abc203

---
+++ abc314/b/main.js
@@ -0,0 +1,12 @@
+var fs = require('fs');
+var input = '';
+process.stdin.on('data', function(data) {
+    input += data;
+});
+process.stdin.on('end', function() {
+    const args = input.split('\n');
+    const nums = args[0].split(' ');
+  
+    console.log(nums);
+
+});
